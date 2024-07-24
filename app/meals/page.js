a0540be5a2e0ec8e4f40e7571e10efcc52f0d2f5
KamileSeidu <kseidu005@st.ug.edu.gh>
2024-07-24 09:23:44 +0000

    added a few helper components to structure the meals page to recieve the neccesaty data

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,25 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage() {
-  return <div>Hello Community!</div>;
+  return (
+    <>
+      <header>
+        <h1>
+          Delicious meals, creatd{""}
+          <span>by you</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun.
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorites Recipe</Link>
+        </p>
+      </header>
+      <main>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }

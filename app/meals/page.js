a316806a558ad911b093068b228d49a71b49f139
KamileSeidu <kseidu005@st.ug.edu.gh>
2024-07-24 13:46:41 +0000

    configured a database and now fetching data inside of the app

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,14 +1,16 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
   return (
     <>
-      <header>
+      <header className={classes.header}>
         <h1>
           Delicious meals, creatd{""}
-          <span>by you</span>
+          <span className={classes.highlight}> by you</span>
         </h1>
         <p>
           Choose your favorite recipe and cook it yourself. It is easy and fun.
@@ -18,7 +20,7 @@ export default function MealsPage() {
         </p>
       </header>
       <main>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
